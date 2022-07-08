a2b62c82948b56c8bec267b94640a3b8641502f7
Caitlyn <caitlyn@oppkey.com>
2022-07-08 07:47:31 -0700

    edited project

--- lib/main.dart
+++ lib/main.dart
@@ -394,8 +394,7 @@ class _PlayerVideoAndPopPageState extends State<_PlayerVideoAndPopPage> {
   void initState() {
     super.initState();
 
-    _videoPlayerController =
-        VideoPlayerController.asset('assets/Butterfly-209.mp4');
+    _videoPlayerController = VideoPlayerController.asset('assets/coffee.MP4');
     _videoPlayerController.addListener(() {
       if (startedPlaying && !_videoPlayerController.value.isPlaying) {
         Navigator.pop(context);
